093e1c2042dd6d9019ee3f42b5c0e999665ccc28
Antonio <antonio@spacebase.one>
2024-09-18 08:50:00 -0700

    [9ntonio][Fix: Analytics updates]

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,30 +30,31 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-google-gtag`,
 			options: {
-				// !! You can add multiple tracking ids and a pageview event will be fired for all of them.
+				// You can add multiple tracking ids and a pageview event will be fired for all of them.
 				trackingIds: [
-					"G-640WERC942", // !! Google Analytics / GA
+					"G-640WERC942", // Google Analytics / GA
+					// "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+					// "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
 				],
-				// !! This object gets passed directly to the gtag config command
-				// !! This config will be shared across all trackingIds
+				// This object gets passed directly to the gtag config command
+				// This config will be shared across all trackingIds
 				gtagConfig: {
-					// optimize_id: "8432808549",
+					optimize_id: "OPT_CONTAINER_ID",
 					anonymize_ip: true,
 					cookie_expires: 0,
 				},
-				// !! This object is used for configuration specific to this plugin
+				// This object is used for configuration specific to this plugin
 				pluginConfig: {
-					// !! Puts tracking script in the head instead of the body
-					head: true,
-
-					// !! Setting this parameter is also optional
+					// Puts tracking script in the head instead of the body
+					head: false,
+					// Setting this parameter is also optional
 					respectDNT: true,
-					// !! Avoids sending pageview hits from custom paths
-					// exclude: ["/secret/**"],
-					// !! Defaults to https://www.googletagmanager.com
+					// Avoids sending pageview hits from custom paths
+					exclude: ["/preview/**", "/do-not-track/me/too/"],
+					// Defaults to https://www.googletagmanager.com
 					origin: "https://antonio.almena.io/",
-					// !! Delays processing pageview events on route update (in milliseconds)
-					// delayOnRouteUpdate: 0,
+					// Delays processing pageview events on route update (in milliseconds)
+					delayOnRouteUpdate: 0,
 				},
 			},
 		},
