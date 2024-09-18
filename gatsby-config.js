13962281c913787932e73fc4f58334cc2262115c
Antonio <antonio@spacebase.one>
2024-09-18 08:59:18 -0700

    [9ntonio][Fix: Analytics updates]

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,20 +13,6 @@ module.exports = {
 		siteUrl: "https://antonio.almena.io",
 	},
 	plugins: [
-		`gatsby-plugin-react-helmet`,
-		`gatsby-plugin-sass`,
-		`gatsby-plugin-image`,
-		`gatsby-plugin-sharp`,
-		`gatsby-transformer-sharp`,
-		{
-			resolve: `gatsby-plugin-google-fonts`,
-			options: {
-				fonts: [
-					`Fredoka\:300,400,500,600,700`, // you can also specify font weights and styles
-				],
-				display: "swap",
-			},
-		},
 		{
 			resolve: `gatsby-plugin-google-gtag`,
 			options: {
@@ -39,18 +25,15 @@ module.exports = {
 				// This object gets passed directly to the gtag config command
 				// This config will be shared across all trackingIds
 				gtagConfig: {
-					optimize_id: "OPT_CONTAINER_ID",
 					anonymize_ip: true,
 					cookie_expires: 0,
 				},
 				// This object is used for configuration specific to this plugin
 				pluginConfig: {
 					// Puts tracking script in the head instead of the body
-					head: false,
-					// Setting this parameter is also optional
-					respectDNT: true,
+					head: true,
 					// Avoids sending pageview hits from custom paths
-					exclude: ["/preview/**", "/do-not-track/me/too/"],
+					// exclude: ["/preview/**", "/do-not-track/me/too/"],
 					// Defaults to https://www.googletagmanager.com
 					origin: "https://antonio.almena.io/",
 					// Delays processing pageview events on route update (in milliseconds)
@@ -58,6 +41,20 @@ module.exports = {
 				},
 			},
 		},
+		{
+			resolve: `gatsby-plugin-google-fonts`,
+			options: {
+				fonts: [
+					`Fredoka\:300,400,500,600,700`, // you can also specify font weights and styles
+				],
+				display: "swap",
+			},
+		},
+		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-sass`,
+		`gatsby-plugin-image`,
+		`gatsby-plugin-sharp`,
+		`gatsby-transformer-sharp`
 	],
 	pathPrefix: "/",
 };
